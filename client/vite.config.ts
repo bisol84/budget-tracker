95c153cbd8d8f0eaa607db6001603e0073c19156
Sebastien Bisoglio <sbisoglio@gmail.com>
2025-02-09 22:40:33 +0100

    bug: added allowedHosts to viet.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    allowedHosts: ["myapp.191284.xyz"], // 🔥 Autorise cet hôte
+  },
+});
