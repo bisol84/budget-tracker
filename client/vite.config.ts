a020b5fe25e66ab7ac61e3bf78f1c0dce4756fd5
Sebastien Bisoglio <sbisoglio@gmail.com>
2025-02-12 16:08:45 +0100

    - added shadcn to client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   preview: {
     allowedHosts: ["myapp.191284.xyz"], // 🔥 Autorise cet hôte
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
