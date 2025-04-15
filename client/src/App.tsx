9085076a191d267606c03cb96d824264fc53b42b
Sebastien Bisoglio <sbisoglio@gmail.com>
2025-04-15 22:13:19 +0200

    Test commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div>
       My first page
       <h1 className="text-3xl font-bold underline bg-red-600">
-        Hell00oo World
+        Hell00oo2 World
       </h1>
     </div>
   );
