1a27e9e616c96affc73230556c8d8bc210207428
Sebastien Bisoglio <sbisoglio@gmail.com>
2025-04-15 19:49:25 +0200

    Test commit

---
+++ client/src/pages/Test1.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur mon application !</h1>
+      <p>Découvrez nos fonctionnalités et explorez les utilisateurs.</p>
+    </div>
+  );
+}
