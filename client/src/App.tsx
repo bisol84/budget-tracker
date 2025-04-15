6b645b444721bef8dd9c23ac803f6f9103363876
Sebastien Bisoglio <sbisoglio@gmail.com>
2025-04-15 22:04:10 +0200

    Test commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       My first page
-      <h1 className="text-3xl font-bold underline bg-red-600">Hello World</h1>
+      <h1 className="text-3xl font-bold underline bg-red-600">
+        Hell00oo World
+      </h1>
     </div>
   );
 }
