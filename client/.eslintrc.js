2ed3ef84191b5e525fce94e05bd7c9ed6b9e1287
Sebastien Bisoglio <sbisoglio@gmail.com>
2025-04-16 19:58:44 +0200

    eslint added tanstack form added

---
+++ client/.eslintrc.js
@@ -0,0 +1,29 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
-import "./App.css";
-import React from "react";
+import './App.css';
 
 function App() {
   return (
     <div>
       My first page
-      <h1 className="text-3xl font-bold underline bg-red-600">
-        Hell00oo2 World
-      </h1>
+      <h1 className="text-3xl font-bold underline bg-red-600">Hell00oo2 World</h1>
     </div>
   );
 }
